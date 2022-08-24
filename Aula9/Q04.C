2efd1e556cc3200c3518d50e57864a7f328f3fd8
Rafaela Bezerra de Figueiredo <82965842+RafaelaBF@users.noreply.github.com>
2022-08-24 14:24:57 -0300

    Update Q04.C

--- Aula9/Q04.C
+++ Aula9/Q04.C
@@ -3,10 +3,7 @@
   Turma: 3BINFO
 */
 
-/*
-Considerando a entrada via teclado de um número na base decimal converta para a base binária.
-(Não vale utilizar a máscara no printf).
-*/
+
 
 #include <stdio.h>
 
@@ -18,22 +15,22 @@ int main(){
 
     int i = 0; // Tamanho do vetor bin
     if(nD/2!=0){ // Se a divição ter zero quer dizer q a 
-        while(nD/2!=0){ 
-            bin[i]=nD%2;
-            if(nD/2==1){
-                bin[i+1]=nD/2;
+        while(nD/2!=0){ // Continua até o numero for menor que 2
+            bin[i]=nD%2; // Pega o resto da divisão de 2 e coloca no vetor.
+            if(nD/2==1){ // Se for igual a 1.. 
+                bin[i+1]=nD/2; // A proximo casa do vertor é a divião em si e para
                 i++;
                 break;
             }else{
-                nD = nD/2;
+                nD = nD/2; // Continua dividindo
             }
             i++;
         }
     }else{
-        bin[0]=nD%2;
+        bin[0]=nD%2; // Pega só o resto
     }
 
-    for(int j = i; j >= 0; j--) {
+    for(int j = i; j >= 0; j--) { // Printa o binario
         printf("%d", bin[j]);
     }
 }
